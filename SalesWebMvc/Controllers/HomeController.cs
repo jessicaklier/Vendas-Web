a0699e0851f23500d3085184d146e5ef5ec08bb8
jessicaklier <jhessicadesouza12@hotmail.com>
2022-11-18 15:00:24 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Aplicativo de Vendas";
+            ViewData["Desenvolvedora"] = "Jessica De Souza";
 
             return View();
         }
