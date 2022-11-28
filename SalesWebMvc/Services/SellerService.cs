7a945601a5fb91e23ec9dbf1a17527ace2e3b649
jessicaklier <jhessicadesouza12@hotmail.com>
2022-11-28 15:02:46 -0300

    Update seller and custom service exception

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Services
 {
@@ -13,7 +14,7 @@ namespace SalesWebMvc.Services
         {
             _contex = contex;
         }
-        
+
         public List<Seller> FindAll()
         {
             return _contex.Seller.ToList();
@@ -32,7 +33,23 @@ namespace SalesWebMvc.Services
             var obj = _contex.Seller.Find(id);
             _contex.Seller.Remove(obj);
             _contex.SaveChanges();
-   
+
+        }
+        public void Update(Seller obj)
+        {
+            if (!_contex.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new KeyNotFoundException("Id not found");
+            }
+            try
+            {
+                _contex.Update(obj);
+                _contex.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
